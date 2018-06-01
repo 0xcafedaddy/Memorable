379797f07df9cd68a2ea6cf1d947d65eaa9af59c
jarvis <wucw@fsmeeting.com>
2018-06-01 18:32:35 +0800

    Create Domain UseCaseProvider

---
+++ Domain/UseCases/UseCaseProvider.swift
@@ -0,0 +1,13 @@
+//
+//  UseCaseProvider.swift
+//  Domain
+//
+//  Created by jarvis on 2018/6/1.
+//  Copyright © 2018年 JarvisWu. All rights reserved.
+//
+
+import Foundation
+
+public protocol UseCaseProvider {
+    
+}
---
+++ Memorable/Application/Application.swift
@@ -0,0 +1,18 @@
+//
+//  Application.swift
+//  Memorable
+//
+//  Created by jarvis on 2018/6/1.
+//  Copyright © 2018年 JarvisWu. All rights reserved.
+//
+
+import Foundation
+import Domain
+import NetworkPlatform
+import RealmPlatform
+
+final class Application {
+    static let shared = Application()
+    
+    
+}
