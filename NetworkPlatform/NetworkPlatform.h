ebf534819dbf9b0bd009c3e8f063daf298c63409
jarvis <wucw@fsmeeting.com>
2018-06-01 15:27:48 +0800

    Classify program folder
    Classify program folder
    Create domain and platform target
    Create unit test

---
+++ NetworkPlatform/NetworkPlatform.h
@@ -0,0 +1,19 @@
+//
+//  NetworkPlatform.h
+//  NetworkPlatform
+//
+//  Created by jarvis on 2018/6/1.
+//  Copyright © 2018年 JarvisWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkPlatform.
+FOUNDATION_EXPORT double NetworkPlatformVersionNumber;
+
+//! Project version string for NetworkPlatform.
+FOUNDATION_EXPORT const unsigned char NetworkPlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkPlatform/PublicHeader.h>
+
+
